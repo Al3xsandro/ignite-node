3e304ef8cb52a55d55b207fb198d9ffdbcefe1eb
Al3xsandro <alexs123655@gmail.com>
2021-12-05 23:37:18 -0300

    chore: setup database connection

--- chapter-3/src/server.ts
+++ chapter-3/src/server.ts
@@ -4,6 +4,8 @@ import swaggerUI from "swagger-ui-express";
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
 
+import "./database";
+
 const app = express();
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
@@ -12,5 +14,5 @@ app.use(express.json());
 app.use(router);
 
 app.listen(3333, () => {
-  console.log("Server is listen");
+  console.log("Server is listener");
 });
