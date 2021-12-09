bafe010a3c350f39701171cb3c0c36ff11b04f9b
Al3xsandro <alexs123655@gmail.com>
2021-12-08 21:10:39 -0300

    chore: add user id on express

--- chapter-3/src/server.ts
+++ chapter-3/src/server.ts
@@ -18,6 +18,7 @@ app.use(express.json());
 app.use(router);
 
 app.use(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   (err: Error, request: Request, response: Response, next: NextFunction) => {
     if (err instanceof AppError) {
       return response.status(err.statusCode).json({
