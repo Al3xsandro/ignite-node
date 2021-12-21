ba3ca714c539d54c5932e5ececffae200b0f7d7b
Al3xsandro <alexs123655@gmail.com>
2021-12-21 15:16:30 -0300

    chore: add new method to create connection with typeorm

--- chapter-4/src/shared/infra/http/server.ts
+++ chapter-4/src/shared/infra/http/server.ts
@@ -1,16 +1,16 @@
 import "reflect-metadata";
 import express, { Request, Response, NextFunction } from "express";
 import "express-async-errors";
-import swaggerUI from "swagger-ui-express";
-
-import "../typeorm";
 import "@shared/container";
+import swaggerUI from "swagger-ui-express";
+import { createConnection } from "typeorm";
 
 import { AppError } from "@errors/AppError";
 
 import swaggerFile from "../../../swagger.json";
 import { router } from "./routes";
 
+createConnection();
 const app = express();
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
@@ -19,7 +19,6 @@ app.use(express.json());
 app.use(router);
 
 app.use(
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   (err: Error, request: Request, response: Response, next: NextFunction) => {
     if (err instanceof AppError) {
       return response.status(err.statusCode).json({
